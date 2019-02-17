20d69e41708f9925d2be369b03d09e0e8529f207
EndriuPC <endriu102.ms@gmail.com>
2019-02-17 21:23:49 +0100

    Added projects.

---
+++ 1_GenericTypes/1_GenericTypes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1_GenericTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
